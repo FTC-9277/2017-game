f7b121a9cadcc02bca71d6a939373e3aa3848b02
aggarwalva <varunneilaggarwal@gmail.com>
2017-10-04 17:15:56 -0400

    Laptop 716 commit test

---
+++ TeamCode/src/main/java/MainTele.java
@@ -0,0 +1,18 @@
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * Created by robotics9277 on 10/4/2017.
+ */
+@TeleOp(name = "Main")
+public class MainTele extends OpMode {
+    @Override
+    public void init() {
+        //Laptop 716 commit
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
