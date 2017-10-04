52b91cdda1af76e9360fec740b3c8b0fa97eb1c0
aggarwalva <varunneilaggarwal@gmail.com>
2017-10-04 17:53:24 -0400

    navX Test Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTele.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.kauailabs.navx.ftc.AHRS;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -8,13 +9,20 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
  */
 @TeleOp(name = "Main")
 public class MainTele extends OpMode {
+    private final int NAVX_DIM_I2C_PORT = 0;
+    private AHRS navx;
+
     @Override
     public void init() {
-        //Laptop 716 commit test 3
+        navx = AHRS.getInstance(hardwareMap.deviceInterfaceModule.get("dim"),
+                NAVX_DIM_I2C_PORT,
+                AHRS.DeviceDataType.kProcessedData);
     }
 
     @Override
     public void loop() {
-
+        telemetry.addData("Yaw", navx.getYaw());
+        telemetry.addData("Roll", navx.getRoll());
+        telemetry.addData("Pitch", navx.getPitch());
     }
 }
