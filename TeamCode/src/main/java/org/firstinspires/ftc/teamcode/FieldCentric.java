afd04519df2b6edc634ac47589b55911eeab7234
aggarwalva <varunneilaggarwal@gmail.com>
2017-10-18 18:21:15 -0400

    Added rotational PID

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentric.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentric.java
@@ -12,18 +12,20 @@ import com.qualcomm.robotcore.hardware.Servo;
  */
 @TeleOp(name = "Field Centric")
 public class FieldCentric extends OpMode {
-    private final int NAVX_DIM_I2C_PORT = 0;
+    private final int NAVX_DIM_I2C_PORT = 1;
     private AHRS navx;
     private boolean navxInitialized, servosInitialized;
 
     DcMotor fLeft, fRight, bLeft, bRight, strafe;
     Servo i1,i2;
-    double x, y, z, angle, mSet,sSet;
+    double x, y, z, angle, mSet,sSet, expAngle, error, errorScalar;
 
     int ticks = 0;
 
     @Override
     public void init() {
+        errorScalar = 0.02;
+
         try{
             navx = AHRS.getInstance(hardwareMap.deviceInterfaceModule.get("dim"), NAVX_DIM_I2C_PORT, AHRS.DeviceDataType.kProcessedData);
             navxInitialized = true;
@@ -59,7 +61,7 @@ public class FieldCentric extends OpMode {
     public void loop() {
         y = Math.abs(gamepad1.left_stick_y) > 0.1 ? gamepad1.left_stick_y : 0;
         x = Math.abs(gamepad1.left_stick_x) > 0.1 ? gamepad1.left_stick_x : 0;
-        z = Math.abs(gamepad1.right_stick_x) > 0.3 ? gamepad1.right_stick_x : 0;
+        z = 0;//Math.abs(gamepad1.right_stick_x) > 0.3 ? gamepad1.right_stick_x : 0;
 
         if(navxInitialized){
             telemetry.addData("Yaw", navx.getYaw());
@@ -68,15 +70,24 @@ public class FieldCentric extends OpMode {
 
             telemetry.addData("Ticks", ticks);
             ticks++;
-            telemetry.update();
+
 
             angle = Math.toRadians(navx.getYaw());
             mSet = (y * Math.cos(angle)) - (x * Math.sin(angle))/1.5;
             sSet = (y * Math.sin(angle)) + (x * Math.cos(angle));
             telemetry.addData("trig test", Math.cos(180));
+
+            if(Math.abs(gamepad1.right_stick_x) > 0.3 && Math.abs(gamepad1.right_stick_y) > 0.3)  expAngle = Math.atan2(gamepad1.right_stick_x, gamepad1.right_stick_y);
+
+            error = expAngle - navx.getYaw();
+
+            telemetry.addData("Expected angle", expAngle);
+            telemetry.addData("Error", error);
+
+            telemetry.update();
         }
 
-        setCapped(mSet - z, mSet + z, sSet);
+        setCapped(mSet - z - (error * errorScalar), mSet + z + (error * errorScalar), sSet);
 
         if(servosInitialized){
             if(gamepad1.right_trigger > 0.1){
@@ -111,10 +122,10 @@ public class FieldCentric extends OpMode {
         if (max < 1) {
             set(leftPower, rightPower, strafePower);
             return;
-        }
-
-        set(leftPower / max, rightPower / max, strafePower / max);
     }
+
+    set(leftPower / max, rightPower / max, strafePower / max);
+}
 
     public static double maxDouble(double... nums) {
         double currMax = Math.abs(nums[0]);
