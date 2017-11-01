47ca12633b5a59dc61285b4895942e2a430004f7
aggarwalva <varunneilaggarwal@gmail.com>
2017-11-01 15:52:38 -0400

    PID position test was added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PositionPIDTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PositionPIDTest.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
@@ -7,24 +8,34 @@ import com.qualcomm.robotcore.hardware.DcMotor;
  * Created by robotics9277 on 10/26/2017.
  */
 
-public class PositionPIDTest extends OpMode{
-    DcMotor fLeft, fRight, bLeft, bRight, strafe;
+public class PositionPIDTest extends LinearOpMode{
+
+    DcMotor fLeft, bLeft, fRight, bRight;
 
     @Override
-    public void init() {
-        fLeft = hardwareMap.get(DcMotor.class, "fLeft");
+    public void runOpMode() throws InterruptedException {
         bLeft = hardwareMap.get(DcMotor.class, "bLeft");
+        fLeft = hardwareMap.get(DcMotor.class, "fLeft");
         fRight = hardwareMap.get(DcMotor.class, "fRight");
         bRight = hardwareMap.get(DcMotor.class, "bRight");
-        strafe = hardwareMap.get(DcMotor.class, "strafe");
+
+        bRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        fLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        fRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        waitForStart();
+
+
     }
 
-    @Override
-    public void loop() {
+    public void reset() {
 
     }
 
-    public void setZero(DcMotor motor) {
+    public void forward(double pow) {
+        while(){
 
+        }
     }
 }
